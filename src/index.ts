7099f0189bc7cc8c2b7812bf5346905e6c673f85
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-05 00:30:26 +0800

    Feat: Automatically import base modal styles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './ModalBase.css';
+
 export { default as Modal } from './Modal';
 export { default as useModal } from './useModal';
 export { ModalProvider, useModalContext } from './ModalContext';
