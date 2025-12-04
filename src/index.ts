0eeadb551ca79528c4472c3f2659a4908b388228
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-05 00:50:12 +0800

    Refactor: Update CSS for responsive units and remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import './ModalBase.css';
-
 export { default as Modal } from './Modal';
 export { default as useModal } from './useModal';
 export { ModalProvider, useModalContext } from './ModalContext';
