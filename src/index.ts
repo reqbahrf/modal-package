3d3d458a77ac5189560585e7d67ff931581144c5
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-05 18:45:17 +0800

    feat: Remove Tailwind CSS dependency and convert styles to native CSS

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './ModalBase.css';
 export { default as Modal } from './Modal';
 export { default as useModal } from './useModal';
 export { ModalProvider, useModalContext } from './ModalContext';
